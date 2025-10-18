596faed742fe22c444b552031e669efad496532f
evanmayur <habibanievan@gmail.com>
2025-10-18 19:29:25 +0530

    Update Flask app to run on port 5001

--- AI_model_server_Flask/app.py
+++ AI_model_server_Flask/app.py
@@ -32,4 +32,4 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)  # Change to 5001 or any other available port
